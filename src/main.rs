ce1c39c9df6fa1e009c2435ea20c6a0422a4c65d
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2021-02-16 16:45:29 -0300

    Add basic web server
    Closes #2
    
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+static ADDRESS: &str = "127.0.0.1";
+static PORT: &str = "8080";
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(status)
+    })
+        .bind(format!("{}:{}", ADDRESS, PORT))?
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn status() -> impl Responder {
+    HttpResponse::Ok().body("OK")
 }
